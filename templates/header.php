2daeac972c8c509ae3e03a3001aeb41a3a8a62d2
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-05-21 21:21:13 -0300

    update files

---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<?php require_once('./config/url.php');?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda - Contatos</title>
+    <!--BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+     <!--CSS-->
+     <link rel="stylesheet" href="<?php echo $BASE_URL?>./css/style.css">
+     <!-- FONT-AWESOME -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a href="">
+                <img src="<?php echo $BASE_URL?>/img/logo_agenda.svg" alt="Agenda">
+            </a> 
+        </nav>
+    </header>

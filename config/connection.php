15646034912feb20966fca502ea496cbab004f42
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-05-22 22:58:12 -0300

    update files

--- config/connection.php
+++ config/connection.php
@@ -4,3 +4,18 @@ $host = "localhost";
 $dbname ="agenda";
 $user = "root";
 $password = "12345";
+
+
+
+try {
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+}catch(PDOException $e) 
+{
+    $error = $e->getMessage("Erro ao conectar com o banco de dados {$dbname}");
+    echo "Erro: $error";
+}

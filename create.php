8bef410ae3ade8b74e75b642bcfa7148e04ca9e2
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-05-30 21:26:15 -0300

    update files

--- create.php
+++ create.php
@@ -1,14 +1,8 @@
  <?php require_once('./config/process.php')?>
- 
- 
  <?php include_once('./templates/header.php');?>
- <h1>Criando contato</h1>
-  <div class=" form-control">
-        <form action="" method="post">
-            <label for="">Nome do Contato</label>
-            <input name="name" type="text">
-            <label for="">Número</label>
-            <input type="" name="phone">
-        </form>
-    </div>
+ <?php include_once("./templates/backbtn.html");?>
+ <div class="container">
+       <?php include_once("./templates/backbtn.html");?>
+    <h1 id="main-title">Criando Contato</h1>
+ </div>
  <?php include_once('./templates/footer.php');?>

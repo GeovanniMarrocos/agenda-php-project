ebd053f82f27fbabf8d7d8197618fa1d92844942
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-06-06 23:30:47 -0300

    update files

--- show.php
+++ show.php
@@ -8,15 +8,12 @@ include_once('./templates/header.php');
 ?>
 
 <div class="container" id="view-contact-container">
-  <?php include_once("./templates/backbtn.html"); ?>
-  <h1 id="main-title"><?php echo $contacts['name'] ?></h1>
-  <p class="bold">Telefone:</p>
-  <p><?php echo $contacts['phone'] ?></p>
-  <p class="bold">Observações:</p>
-  <p><?php echo $contacts['observations'] ?></p>
-  <div class="foto-perfil">
-    <img src="<?php echo $BASE_URL ?>/img/agenda.svg" alt="Agenda">
-  </div>
+    <?php include_once("./templates/backbtn.html"); ?>
+    <h1 id="main-title"><?php echo $contacts['name'] ?></h1>
+    <p class="bold">Telefone:</p>
+    <p><?php echo $contacts['phone'] ?></p>
+    <p class="bold">Observações:</p>
+    <p><?php echo $contacts['observations'] ?></p>
 </div>
 <?php
 include_once('./templates/footer.php');
